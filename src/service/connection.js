13790a59484001a726050f7ecaf6f4433ccad06f
Akanoshi <juninho.jr.2003@hotmail.com>
2023-02-16 21:04:45 -0300

    adjusted requisition links

--- src/service/connection.js
+++ src/service/connection.js
@@ -3,17 +3,17 @@ import axios from "axios"
 export class RequisitionAxios {
 
    static async Get(path) {
-        return await axios.get(`http://localhost:3000${path}`)
+        return await axios.get(`https://servidornovo.onrender.com${path}`)
     }
     static async Post(path, body) {
-        return await axios.post(`http://localhost:3000${path}`,
+        return await axios.post(`https://servidornovo.onrender.com${path}`,
         body)
     }
     static async Put(path, body) {
-        return await axios.put(`http://localhost:3000${path}`,
+        return await axios.put(`https://servidornovo.onrender.com${path}`,
         body)
     }
     static async Delete(path) {
-        return await axios.delete(`http://localhost:3000${path}`)
+        return await axios.delete(`https://servidornovo.onrender.com${path}`)
     }
 }
